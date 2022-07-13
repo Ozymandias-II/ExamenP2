45aadc4f79e73500fa375945e2c6fefcb623ff1e
Ozymandias-II <jordy_alexander_vargas@outlook.com>
2022-07-13 18:54:42 -0500

    Estructura de proyecto para el examen

---
+++ src/test/java/ec/edu/espe/vargasexamenp2/VargasExamenP2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.vargasexamenp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VargasExamenP2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
